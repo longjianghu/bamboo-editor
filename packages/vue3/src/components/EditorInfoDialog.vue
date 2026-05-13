d668bae39b4972dc611d51bc083a389a5bd14be8
longjianghu <215241062@qq.com>
2026-05-13 08:51:23 +0800

    feat:调整重构后的版本号

--- packages/vue3/src/components/EditorInfoDialog.vue
+++ packages/vue3/src/components/EditorInfoDialog.vue
@@ -104,7 +104,7 @@ onBeforeUnmount(() => {
           </div>
 
           <div class="editor-info-dialog__meta">
-            <span class="editor-info-dialog__version">v{{ pkg ? pkg.version : '0.1.5' }}</span>
+            <span class="editor-info-dialog__version">v{{ pkg ? pkg.version : '0.1.6' }}</span>
             <span class="editor-info-dialog__meta-sep">·</span>
             <a href="mailto:215241062@qq.com" class="editor-info-dialog__author">龙江湖</a>
           </div>
